8abe70f43718f5a24b0dbd0969a98d34baea4b27
GoninDS <luisdi2509@gmail.com>
2024-12-11 16:22:32 -0600

    Initial navbar implementation

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,3 @@
-// Help us dynamically import images
 export const getImageUrl = (path) => {
-    // Construct absolute path of image
-    return new URL(`assets/${path}`, import.meta.url).href;
+    return new URL(`/assets/${path}`, import.meta.url).href;
 };
