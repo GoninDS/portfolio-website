aef62d2e45ad24584fe1a3344be767f568601f56
GoninDS <luisdi2509@gmail.com>
2024-12-11 20:20:31 -0600

    Navbar almost complete

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,13 +5,24 @@ import { getImageUrl } from "../../utils";
 
 
 export const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(true)
+
     // Semantic html tag => div => nav
     return <nav className={styles.navbar}>
         <a className={styles.title} href="/">
             Portfolio
         </a>
         <div className={styles.menu}>
-            <img className={styles.menuBtn} src={getImageUrl("nav/menuIcon.png")}/>
+            <image 
+                className={styles.menuBtn} 
+                src={
+                    menuOpen 
+                        ? getImageUrl("closeIcon.png")
+                        : getImageUrl("menuIcon.png")
+                }
+                alt="menu-button"
+                onClick={() => setMenuOpen(!menuOpen)}
+            />
             <ul className={styles.menuItems}>
                 <li>
                     <a href="#about">About</a>
